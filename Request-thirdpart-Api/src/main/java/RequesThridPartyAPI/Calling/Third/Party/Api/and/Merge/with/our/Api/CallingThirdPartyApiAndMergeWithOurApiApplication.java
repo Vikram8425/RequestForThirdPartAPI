bb53b78002cb49b87cab89629981d3b724eebafa
vikram8425 <pmsecm@gmail.com>
2025-05-22 15:54:55 +0530

    Thrid API Integaration

---
+++ Request-thirdpart-Api/src/main/java/RequesThridPartyAPI/Calling/Third/Party/Api/and/Merge/with/our/Api/CallingThirdPartyApiAndMergeWithOurApiApplication.java
@@ -0,0 +1,13 @@
+package RequesThridPartyAPI.Calling.Third.Party.Api.and.Merge.with.our.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingThirdPartyApiAndMergeWithOurApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingThirdPartyApiAndMergeWithOurApiApplication.class, args);
+	}
+
+}
---
+++ Request-thirdpart-Api/src/test/java/RequesThridPartyAPI/Calling/Third/Party/Api/and/Merge/with/our/Api/CallingThirdPartyApiAndMergeWithOurApiApplicationTests.java
@@ -0,0 +1,13 @@
+package RequesThridPartyAPI.Calling.Third.Party.Api.and.Merge.with.our.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallingThirdPartyApiAndMergeWithOurApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
